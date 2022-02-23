844641bf371374a0b701e7adf1b1967cfdbaea44
DVMorozovv <dimon_moroz2@mail.ru>
2022-02-23 18:41:55 +0700

    Procfile for Heroku

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -30,23 +30,50 @@ class ProfileController extends Controller
         return $filesize.$formats[$format];
     }
     public function files(Request $req){
-        $user = Auth::user();
-        $user_id = $user->id;
-        $files = new File();
-        $files = DB::table('files')->where('user_id', '=', "$user_id")->get();
+//        $user = Auth::user();
+//        $user_id = $user->id;
+//        $files = new File();
+//        $files = DB::table('files')->where('user_id', '=', "$user_id")->get();
+//
+//
+//        return view('pages/user-files',  ['files'=>$files]);
 
-
-        return view('pages/user-files',  ['files'=>$files]);
-
-//        $filename = 'files/items.xlsx';
-//        $s = self::filesize_format(filesize($filename));
-//        $file = new File();
-//        $file->name = "items.xlsx";
-//        $file->path = "$filename";
-//        $file->user_id = '2';
-//        $file->size = "$s";
-//        $file->save();
-//        echo $s;
+        $filename = 'files/items.xlsx';
+        $s = self::filesize_format(filesize($filename));
+        $file = new File();
+        $file->name = "items.xlsx";
+        $file->path = "$filename";
+        $file->user_id = '0';
+        $file->size = "$s";
+        $file->save();
+        echo $s;
+        $filename = 'files/items.xlsx';
+        $s = self::filesize_format(filesize($filename));
+        $file = new File();
+        $file->name = "items.xlsx";
+        $file->path = "$filename";
+        $file->user_id = '1';
+        $file->size = "$s";
+        $file->save();
+        echo $s;
+        $filename = 'files/items.xlsx';
+        $s = self::filesize_format(filesize($filename));
+        $file = new File();
+        $file->name = "items.xlsx";
+        $file->path = "$filename";
+        $file->user_id = '0';
+        $file->size = "$s";
+        $file->save();
+        echo $s;
+        $filename = 'files/items.xlsx';
+        $s = self::filesize_format(filesize($filename));
+        $file = new File();
+        $file->name = "items.xlsx";
+        $file->path = "$filename";
+        $file->user_id = '1';
+        $file->size = "$s";
+        $file->save();
+        echo $s;
 
     }
 }
